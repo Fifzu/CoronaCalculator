649c3007a1b0acad1352995c1a7487ed07d6da50
Fifzu <friedrich.albrecht@yahoo.com>
2020-03-14 22:03:47 +0100

    DataReader added

--- src/CoronaCalculator.java
+++ src/CoronaCalculator.java
@@ -4,24 +4,29 @@ import java.io.IOException;
 public class CoronaCalculator {
     static int days;
     static double deaths;
-    static int population = 327000000;
-    static double prozent;
-    static FileWriter myWriter;
+    static int population = 1360000000;
+    static double percent;
+    static FileWriter fileWriter;
+    static DataReader dataReader;
 
     public static void main(String[] args) throws IOException {
 
-        myWriter = new FileWriter("corona.csv");
-try {
-    myWriter.write("Infizierte;Tote" + "\n");
-    double x = 20000;
-    x = calculate(x);
-    calculateDecrease(x);
-    myWriter.close();
+        dataReader = new DataReader("South Korea");
 
-} catch (IOException e) {
-    System.out.println("An error occurred.");
-    e.printStackTrace();
-    }
+
+//        fileWriter = new FileWriter("corona.csv");
+//
+//        try {
+//            fileWriter.write("Infizierte;Tote" + "\n");
+//            double x = 287300;
+//            x = calculate(x);
+//            calculateDecrease(x);
+//            fileWriter.close();
+//
+//        } catch (IOException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
     }
 
     private static double calculate(double x) throws IOException {
@@ -29,11 +34,11 @@ try {
         if (x < population * 0.66) {
             x = x * 1.3;
             days++;
-            prozent = (x / population) * 100;
+            percent = (x / population) * 100;
             deaths = x * 0.007;
-            System.out.println("In " + days + " Tagen sind " + (int) x + " Personen infiziert (" + (int) prozent + "%). "+ (int) deaths + " sind tot.");
+            System.out.println("In " + days + " Tagen sind " + (int) x + " Personen infiziert (" + (int) percent + "%). "+ (int) deaths + " sind tot.");
 
-            myWriter.write((int) x + ";" + (int) deaths + "\n");
+            fileWriter.write((int) x + ";" + (int) deaths + "\n");
 
             return calculate(x);
         } else {
@@ -47,11 +52,11 @@ try {
         if (x > 10) {
             x = (x * 0.7);
             days++;
-            prozent = (x / population) * 100;
+            percent = (x / population) * 100;
             deaths = deaths + x * 0.007;
-            System.out.println("In " + days + " Tagen sind " + (int) x + " Personen infiziert (" + (int) prozent + "%). " + (int) deaths + " sind tot.");
+            System.out.println("In " + days + " Tagen sind " + (int) x + " Personen infiziert (" + (int) percent + "%). " + (int) deaths + " sind tot.");
 
-            myWriter.write((int) x + ";" + (int) deaths + "\n");
+            fileWriter.write((int) x + ";" + (int) deaths + "\n");
 
             return calculateDecrease(x);
 
---
+++ src/DataReader.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DataReader {
+
+    static FileWriter fileWriter;
+    public DataReader(String country) {
+        List<String> data;
+
+        data = getData(country);
+
+        printData(data,country);
+    }
+
+    private List<String> getData (String country) {
+        List<String> records = new ArrayList<>();
+        File[] fileList = new File("../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports").listFiles();
+        Arrays.sort(fileList);
+
+        for (File file : fileList) {
+            String filecheck = file.getName().substring(file.getName().length()-4);
+            if (filecheck.contains(".csv"))
+            {
+                try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                    int infected = 0;
+                    String line = "";
+                    while (true) {
+                        try {
+                            if (!((line = br.readLine()) != null)){
+                                records.add(file.getName().replaceFirst("[.][^.]+$", "") + ";"+infected);
+                                break;
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        String[] values = line.split(",");
+
+                        if (values[1].equals(country) && values.length>3) {
+                            infected += Integer.parseInt(values[3]);
+
+                        }
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return records;
+    }
+
+    private void printData (List<String> records,String country)  {
+
+        try {
+            fileWriter = new FileWriter(country + ".csv");
+            fileWriter.write("Datum;Infizierte" + "\n");
+            for (int i=0;i<records.size();i++) {
+                System.out.println(records.get(i));
+                fileWriter.write(records.get(i)+"\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
